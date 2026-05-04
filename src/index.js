36a9b9d6405c4213515c62cc25045084d1b56a7b
esraspringintveld <esraspringintveld@gmail.com>
2026-05-04 10:58:26 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('SW registered'))
+      .catch(err => console.log('SW failed:', err));
+  });
+}
