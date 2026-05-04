9cc952c238af9eab85b5b098fa7a491b4dca6695
esraspringintveld <esraspringintveld@gmail.com>
2026-05-04 10:57:45 +0200

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'health-reset-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
