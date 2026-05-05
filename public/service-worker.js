4f97086d2bc3262582f96ce4f6e978e9a62abdfa
esraspringintveld <esraspringintveld@gmail.com>
2026-05-05 10:55:02 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,5 @@
-const CACHE_NAME = 'health-reset-v1';
-const urlsToCache = ['/'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
-
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => clients.claim());
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
-  );
+  event.respondWith(fetch(event.request));
 });
